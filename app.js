e5038bb1659d8bf3452ed7e425b740a8d5785a23
BernardoG <A00570682@itesm.mx>
2021-05-19 13:42:52 -0500

    Server, routes and static files added

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+
+/**
+ * Database connection
+ */
+
+
+/**
+ * Routes
+ */
+const indexRoutes = require('./routes/router')
+app.use('/', indexRoutes);
+
+
+/**
+ * Middlewares
+ */
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+//Middleware para subir archivos estaticos
+app.use(express.static('styles'));
+app.use(express.static('pictures'));
+
+/**
+ * Settings
+ */
+app.set('port', process.env.PORT || 8080);
+// app.set('styles', path.join(__dirname,'styles'));
+
+/**
+ * App listening
+ */
+app.listen(app.get('port'), () =>{
+    console.log(`Server on port ${app.get('port')}`);
+})
+
+
